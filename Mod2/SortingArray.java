63f12a1503247b989e2db7724ecd388cf3db22d6
jose-msudenver <tarotorres023@gmail.com>
2025-09-14 19:23:21 -0600

    Add files via upload

---
+++ Mod2/SortingArray.java
@@ -0,0 +1,20 @@
+package cs2050m02;
+
+public class SortingArray {
+
+	public static void main(String[] args) {
+		
+		for(int i = j+1; j<list.lenght; i++) {
+			if(currentMin > list[i]) {
+				currentMin=list[i];
+				currentMinIndex = i;
+				
+			}
+		}
+		
+		
+		
+
+	}
+
+}
